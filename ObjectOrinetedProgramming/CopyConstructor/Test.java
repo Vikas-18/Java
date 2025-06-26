67f6d3895759acca5332beed440be590c320ea43
Vikas-18 <vikaskumar2k24ee@gmail.com>
2025-06-26 22:45:00 +0530

    Object Oriented Programming copy constructor

---
+++ ObjectOrinetedProgramming/CopyConstructor/Test.java
@@ -0,0 +1,34 @@
+package ObjectOrinetedProgramming.CopyConstructor;
+
+public class Test {
+    public static class Student{
+        String name;
+        int marks;
+
+        Student()
+        {
+            this.name = "Vikas";
+            this.marks = 100;
+        }
+
+        Student(String name, int marks)
+        {
+            this.name = name;
+            this.marks = marks;
+        }
+
+
+        Student(Student other)
+        {
+            this.marks = other.marks;
+            this.name = other.name;
+        }
+
+    }
+    public static void main(String[] args) {
+        Student student = new Student("Vikas",100);
+        Student student1 = new Student(student);
+
+        System.out.println(student1.name);
+    }
+}
